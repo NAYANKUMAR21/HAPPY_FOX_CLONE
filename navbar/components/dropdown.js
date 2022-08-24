20c26385186d0b23a40885950c228c0e0cb0c36c
Nayan Kumar <naayaankumar@gmail.com>
2022-08-24 21:07:24 +0530

    updated day-2

---
+++ navbar/components/dropdown.js
@@ -0,0 +1,137 @@
+{/* <div id="navbar">
+    <ul>
+      <li>features</li>
+      <li>customer</li>
+      <li>price</li>
+      <li>Resources</li>
+    </ul>
+  </div> */}
+function drop() {
+  return `    
+  <div id="parentS">
+    <div id="service">
+      <div>
+        <p>For Support Teams</p>
+        <p>
+          Boost your support <br />
+          operations and ensure <br />
+          happy customers
+        </p>
+      </div>
+      <div id="grid">
+        <div>
+          <span class="servicePara">SUPPORT</span>
+          <div>
+            <div>
+              <img
+                src="https://assets.www.happyfox.com/v2/images/site-nav/menu-logo-helpdesk.svg"
+                alt="helpdesk"
+              />
+            </div>
+            <div><p class="tag">Help desk ></p></div>
+          </div>
+          <p class="servicePara">
+            All-in-one help desk ticketing system to <br />
+            provide better support for your customers
+          </p>
+        </div>
+        <div>
+          <span class="servicePara">AUTOMATION AND TECHNOLOGY</span>
+          <div>
+            <div>
+              <img
+                src="https://assets.www.happyfox.com/v2/images/site-nav/menu-logo-workflows.svg"
+                alt="helpdesk"
+              />
+            </div>
+            <div><p class="tag">Workflow ></p></div>
+          </div>
+          <p class="servicePara">
+            Ensure speedy resolution of tickets with <br />
+            end-to-end process automation
+          </p>
+        </div>
+        <div>
+          <span class="servicePara">CHAT SOLUTION</span>
+          <div>
+            <div>
+              <img
+                src="https://assets.www.happyfox.com/v2/images/site-nav/menu-logo-livechat.svg"
+                alt="helpdesk"
+              />
+            </div>
+            <div><p class="tag">Live Chat</p></div>
+          </div>
+          <p class="servicePara">
+            chat with customers instantly on <br />
+            mobile,desktop and web
+          </p>
+        </div>
+        <div>
+          <span class="servicePara">REPORTING AND ANALYTICS</span>
+          <div>
+            <div>
+              <img
+                src="https://assets.www.happyfox.com/v2/images/site-nav/menu-logo-bi.svg"
+                alt="helpdesk"
+              />
+            </div>
+            <div><p class="tag">Business & Intelligence</p></div>
+          </div>
+          <p class="servicePara">
+            Make data-driven decisions & better serve <br />
+            your customers with reports and analytics
+          </p>
+        </div>
+        <div>
+          <span class="servicePara">AI SOLUTION</span>
+          <div>
+            <div>
+              <img
+                src="https://assets.www.happyfox.com/v2/images/site-nav/menu-logo-chatbot.svg"
+                alt="helpdesk"
+              />
+            </div>
+            <div><p class="tag">Chatbot</p></div>
+          </div>
+          <p class="servicePara">
+            Engage and reach your customers 24x7 <br />
+            with ready-to-go chatbots
+          </p>
+        </div>
+        <div>
+          <span class="servicePara">MOBILE SOLUTION</span>
+          <div>
+            <div>
+              <img
+                src="https://assets.www.happyfox.com/v2/images/site-nav/menu-logo-inapp-chat.svg"
+                alt="helpdesk"
+              />
+            </div>
+            <div><p class="tag">Chatbot</p></div>
+          </div>
+          <p class="servicePara">
+           
+              Manage tickets, collaborate with ease, <br> and track performance on
+              the move
+          
+          </p>
+        </div>
+      </div>
+      <div id="story">
+          <span class="servicePara">CUSTOMER STORY</span>
+          <div>
+              <img
+                src="https://assets.www.happyfox.com/v2/images/site-nav/menu-show-item-3.jpg"
+                alt="helpdesk"
+              />
+            </div>
+          <p class="servicePara">
+              HappyFox knows the next “big thing” <br> in customer support
+          </p>
+      </div>
+    </div>
+  </div>`;
+}
+export { drop };
+
